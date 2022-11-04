39bc5c0bf439fe45d56cb99e76b937cbd5feb9a2
Emanuel Ramponi <emanuel.ramponi1@gmail.com>
2022-11-03 22:06:16 -0300

    second commit

--- src/Caracteres.java
+++ src/Caracteres.java
@@ -1,9 +1,12 @@
 public class Caracteres {void main(String[] args) {
         
         char caracter = '\u0040';
-        char decimal = 64;
+        var decimal = 64;
         System.out.println("caracter: " +caracter);
         System.out.println("caracter: " + decimal);
         System.out.println("caracter = decimal? : " + (caracter == decimal));
+
+        char simbolo = '@';
+        System.out.println("$simbolo = " + (simbolo == caracter));
     }
 }
