f6d3aacd9feb60d19af918dcad38cf6d601de586
Emanuel Ramponi <emanuel.ramponi1@gmail.com>
2022-11-03 23:07:43 -0300

    Primitivos

---
+++ src/PrimitivosCaracteres.java
@@ -0,0 +1,19 @@
+public class PrimitivosCaracteres{
+        public static void main(String[] args) {
+            
+            char caracter = '\u0040';
+            var decimal = 64;
+            System.out.println("caracter: " +caracter);
+            System.out.println("caracter: " + decimal);
+            System.out.println("caracter = decimal? : " + (caracter == decimal));
+    
+            char simbolo = '@';
+            System.out.println("$simbolo = " + (simbolo == caracter) + "\n");
+
+            System.out.println("char corresponde en byte = " + Character.BYTES); // System.getProperty("line.separator") || System.lineSeparator = \n
+            System.out.println("char corresponde en bites a " + Character.SIZE);
+            System.out.println("Character.MIN_VALUE = " + Character.MIN_VALUE);
+            System.out.println("Character.MAX_VALUE = " + Character.MAX_VALUE );
+        }
+    }
+
--- src/PrimitivosEnteros.java
+++ src/PrimitivosEnteros.java
@@ -1,5 +1,4 @@
-public class HelloWorld {
-    static float varFlotante;
+public class PrimitivosEnteros {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD\n");
 
@@ -35,24 +34,7 @@ public class HelloWorld {
         System.out.println("valor maximo de un byte: " + Long.MAX_VALUE);
         System.out.println("valor minimo de un byte: " + Long.MIN_VALUE + "\n");
      
-        float realFloat = 2.12e3f; //2120f
-        realFloat = 1.5e-10f; //0.00000000015f
-        realFloat = 0.00000000015f;
-        System.out.println("real float = " + realFloat);
-        System.out.println("float corresponde en byte a " + Float.BYTES);
-        System.out.println("float corresponde en bites a " + Float.SIZE);
-        System.out.println("Maximo valor para float = " + Float.MAX_VALUE);
-        System.out.println("Minimo valor para float = " + Float.MIN_VALUE  + "\n");
-
-        double realDouble = 3.4028235E39;
-        System.out.println("real double = " + realDouble);
-        System.out.println("double corresponde en byte a " + Double.BYTES);
-        System.out.println("double corresponde en bites a " + Double.SIZE);
-        System.out.println("Maximo valor para double = " + Double.MAX_VALUE);
-        System.out.println("Minimo valor para double = " + Double.MIN_VALUE  + "\n");
         
-
-        System.out.println("varFlotante: " + varFlotante);
 
     }
 }
