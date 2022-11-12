db39732109cbaebe0c51556b0ff1ca2dc3c1eb78
Emanuel Ramponi <emanuel.ramponi1@gmail.com>
2022-11-12 00:01:28 -0300

    Sistemas Numericos Entrada Scanner

---
+++ src/SistemasNumericosEntradaScanner.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SistemasNumericosEntradaScanner {
+    public static void main(String[] args) {
+            //otro metodo para pasar a int mediante scanner y por medio de otra exception  
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese un numero entero: ");
+            // String numeroStr = scanner.nextLine();
+            
+            int numeroDecimal = 0; 
+            try {
+
+                numeroDecimal = scanner.nextInt(); // numeroDecimal = Integer.parseInt(numeroStr); 
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Error! Debe ingresar un numero");
+                main(args);
+                System.exit(0); 
+
+            } 
+
+            scanner.close();
+
+            System.out.println("numeroDecimal = " + numeroDecimal);
+
+            String mensaje = "numero Binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal) + "\n" +
+            "numero octal de " + numeroDecimal +  " = " + Integer.toOctalString(numeroDecimal) + "\n" +
+            "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal) ;
+
+            System.out.println(mensaje);
+    }
+}
