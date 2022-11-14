50df4caec0643f52ce6035d52d8b7e251301c8d8
Emanuel Ramponi <emanuel.ramponi1@gmail.com>
2022-11-13 23:39:09 -0300

    Terminando de ver variables

---
+++ src/ConversionDeTipos.java
@@ -0,0 +1,56 @@
+public class ConversionDeTipos {
+    public static void main(String[] args) {
+
+        //CONVERSION DE CADENAS A PRIMITIVOS
+        String numeroStr = "50";
+
+        int numeroInt = Integer.parseInt(numeroStr);
+        System.out.println("numeroInt = " + numeroInt);
+
+        String realStr = "328943298.32e-3";
+        double realDouble = Double.parseDouble(realStr);
+        System.out.println("realDouble = " + realDouble);
+
+        String logicoStr = "TruE";
+        boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
+        System.out.println("logicoBoolean = " + logicoBoolean);
+
+        //CONVERSION DE PRIMITIVOS A CADENAS
+        //Hay dos maneras de conversion
+
+        int otroNumeroInt = 100;
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+        //1° MANERA
+        //Realizamos la conversion
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+        
+        //2° MANERA
+        otroNumeroStr = String.valueOf(otroNumeroStr + 10); // A este metodo se lo conoce como sobrecarga de metodos
+                                                            // si le agregamos signos en un string va a tomar el valor con la operacion deseada
+                                                            // si en vez de int es string, va a concatenar, no a sumar
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        double otroRealDouble = 1.234323e2;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        //CONVERSION DE PRIMITIVOS A PRIMITIVOS
+        int i = 12769; //cuando supera el limite de short y lo forzamos a pasarlo a short,va a haber perdida de informacion si supera el limite
+        short s = (short) i; //a este metodo de conversion se lo llama casting/cast
+        System.out.println("s = " + s);
+        System.out.println(Short.MAX_VALUE);
+
+        long l = i;
+        System.out.println("l = " + l);
+
+        char b = (char) i;
+        System.out.println("b = " + b);
+
+        float f = (float) i;
+        System.out.println("f = " + f);
+
+        
+        // EN PRIMITIVOS SOLAMENTE SE PUEDE HACER CAST A VALORES DE TIPO NUMERICOS Y TAMBIEN CHAR
+    }
+}
---
+++ src/EjercicioOpcional.java
@@ -0,0 +1,22 @@
+public class EjercicioOpcional {
+    public static void main(String[] args) {
+        /*
+         * La tarea consiste en crear una nueva clase con el metodo main llamada DetalleDeFactura, 
+         * se requiere desarrollar un programa que reciba datos de la factura utilizando la clase Scanner
+         * de la siguiente manera:
+         * 1. Reciba el nombre de la factura o descripcion, utilizar metodo nextLine() para obtener el nombre
+         *    de la factura con espacios
+         * 2. Reciba 2 precios de productos de tipo double, utilizar metodo nextDouble() para obtener precios
+         *    con decimales
+         * 3. Calcule el total, sume ambos precios y agregue un valor de impuesto del 19%
+         * 
+         * Se pide mostrar en un solo String el nombre de la factura, el monto total bruto (antes de impuesto),
+         * el impuesto y el monto total neto incluyendo impuesto
+         * 
+         * Por ejemplo algo asi:
+         * "La factura producto de oficina tiene un total bruto de 134.78, con un impuesto de 25.6082, y el
+         * monto despues de impuesto es de 160.3882"
+         */
+        
+    }
+}
